45371bf5ccfeadae645c120d5aebb22eb582b939
nabendu82 <nabendu.biswas@gmail.com>
2022-04-25 16:57:43 +0530

    Day-13

--- DOM-Basics/main.js
+++ DOM-Basics/main.js
@@ -17,12 +17,48 @@
 // }
 
 //DOM manipulation
-const ul = document.querySelector('.items');
+// const ul = document.querySelector('.items');
 // ul.remove()
-ul.firstElementChild.textContent = 'HTML';
-ul.children[1].innerText = 'CSS';
-ul.lastElementChild.innerHTML = '<h1>JavaScript</h1><p>JS is Cool</p>';
+// ul.firstElementChild.textContent = 'HTML';
+// ul.children[1].innerText = 'CSS';
+// ul.lastElementChild.innerHTML = '<h1>JavaScript</h1><p>JS is Cool</p>';
 
-const button = document.querySelector('.btn');
-button.style.backgroundColor = 'blue';
-button.style.textTransform = 'uppercase';
+// const button = document.querySelector('.btn');
+// button.style.backgroundColor = 'blue';
+// button.style.textTransform = 'uppercase';
+
+//Event Listener
+// const btn = document.querySelector('.btn');
+// const ul = document.querySelector('.items');
+
+// btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     ul.firstElementChild.textContent = 'HTML';
+//     ul.children[1].innerText = 'CSS';
+//     ul.lastElementChild.innerHTML = '<h1>JavaScript</h1><p>JS is Cool</p>';
+// })
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+//Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerText = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const listItem = document.createElement('li');
+        const textNode = document.createTextNode(`${nameInput.value}: ${emailInput.value}`);
+        listItem.appendChild(textNode);
+        userList.appendChild(listItem);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
