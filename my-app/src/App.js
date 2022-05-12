f6d0b881cc46ca9f05c5cb3e158f6e004cf84c96
nabendu82 <nabendu.biswas@gmail.com>
2022-05-12 17:00:35 +0530

    Day-2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
+import EventBind from './components/EventBind';
 // import { Greet, ReactGreet } from './components/Greet';
-import Greeting from './components/Greeting';
-import Welcome from './components/Welcome';
+// import Greeting from './components/Greeting';
+// import Welcome from './components/Welcome';
+// import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       {/* <Greet />
       <ReactGreet /> */}
-      <Greeting name="JavaScript" />
+      {/* <Greeting name="JavaScript" />
       <Greeting name="Golang" />
       <Greeting name="Swift">Language for iOS</Greeting>
       <hr />
       <Welcome name="Cobol" />
       <Welcome name="Kotlin" />
-      <Welcome name="Java"><button>Click</button></Welcome>
+      <Welcome name="Java"><button>Click</button></Welcome> */}
+      {/* <Counter /> */}
+      <EventBind />
     </div>
   );
 }
