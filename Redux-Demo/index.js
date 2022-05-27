c69c7df04a40ef185db84b9218d216a29728066c
nabendu82 <nabendu.biswas@gmail.com>
2022-05-27 16:46:04 +0530

    Day 9

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,6 +1,11 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 const BUY_EGG = 'BUY_EGG';
+const BUY_CHICKEN = 'BUY_CHICKEN';
 
 function buyEgg() {
     return {
@@ -9,11 +14,22 @@ function buyEgg() {
     };
 }
 
-const initialState = {
-    numOfEggs: 100
+function buyChicken() {
+    return {
+        type: BUY_CHICKEN,
+        info: 'Buying Chicken'
+    };
 }
 
-const reducer = (state = initialState, action) => {
+const initialEggState = {
+    numOfEggs: 100,
+}
+
+const initialChickenState = {
+    numOfChickens: 10
+}
+
+const eggReducer = (state = initialEggState, action) => {
     switch(action.type) {
         case BUY_EGG: return {
             ...state,
@@ -23,9 +39,25 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer);
+const chickenReducer = (state = initialChickenState, action) => {
+    switch(action.type) {
+        case BUY_CHICKEN: return {
+            ...state,
+            numOfChickens: state.numOfChickens - 1
+        }
+        default: return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    egg: eggReducer,
+    chicken: chickenReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial State', store.getState());
-const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyEgg());
 store.dispatch(buyEgg());
+store.dispatch(buyChicken());
 unsubscribe();
